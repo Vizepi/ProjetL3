35268444f850a575d1b59f9449b7fdc5d7323c01
Vizepi <joseph.kieffer@edu.univ-fcomte.fr>
2014-11-13 17:31:39 +0100

    Added main.cpp file to project

---
+++ ProjetL3/src/main.cpp
@@ -0,0 +1,35 @@
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char** argv)
+{
+	sf::RenderWindow window(sf::VideoMode(1280, 1024), "Platformer");
+	sf::Clock frameTimer;
+	window.setVerticalSyncEnabled(true);
+
+	while(window.isOpen())
+	{
+		sf::Event event;
+		while(window.pollEvent(event))
+		{
+			switch(event.Type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			default:
+				break;
+			}
+		}
+		// Begin Update
+
+		// End Update
+
+		window.clear(sf::Color::Black);
+
+		// Begin Render
+
+		// End Render
+
+		window.display();
+	}
+}
