35ed111aa91c66566d4092225689d31da8315bf4
lisa2905 <lisa.corbat@hotmail.fr>
2015-01-16 09:46:16 +0100

    Terminated procedural generation

--- ProjetL3/src/main.cpp
+++ ProjetL3/src/main.cpp
@@ -17,14 +17,14 @@ int main(int argc, char** argv)
 	//Chargement du level test.
 	level.CreateTestLevel();
 
-	//level.GenerateLevel();
+	level.GenerateLevel();
 
 	//creation du monde dans box2d.
 	level.LoadLevelArray();
 	sf::Clock frameTime;
-	// Debug view pour voir toute la map.
-	//sf::View view(sf::FloatRect(0, 0, BLOC_SIZE * ROOM_WIDTH * 6, BLOC_SIZE * ROOM_HEIGHT * 6));
-	//window.setView(view);
+	 //Debug view pour voir toute la map.
+	sf::View view(sf::FloatRect(0, 0, BLOC_SIZE * ROOM_WIDTH * LEVEL_WIDTH, BLOC_SIZE * ROOM_HEIGHT * LEVEL_HEIGHT));
+	window.setView(view);
 	//Tant que la fenetre est ouverte.
 	while(window.isOpen())
 	{
